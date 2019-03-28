41b748813177cc1cd0bc2cec06f12f30e306462b
andriesbron <andries@inourplace.nl>
2019-03-28 13:43:31 +0100

    Update omml.class.php

--- OMML/omml.class.php
+++ OMML/omml.class.php
@@ -7,6 +7,27 @@ class omml
   private $copyright;
   private $access = "public";
   
+  /**
+   * @todo create a static function that renders statically, using function parameters. 
+   */
+  public function getOmmlByJson()
+  {
+    
+  }
+  
+  public function renderJsonOmml($guid, $src, $copyright, $access)
+  {
+    
+  }
+  
+  public function setAccessLevel($level)
+  {
+    if (in_array($this->getAccessLevels(), $level) {
+      $this->access = $level;
+      return $this->access;
+    } else return False;
+  }
+  
   public static function getAccessLevels()
   {
     return array(
@@ -14,6 +35,16 @@ class omml
       , "registered"
       , "subscription"
       , "contact"
+    );
+  }
+  
+  public static function getCopyrightOptions()
+  {
+    return array(
+      "name1" => ""
+      , "name2" => ""
+      , "name3" => ""
+      , "name4" => ""
     );
   }
 }
