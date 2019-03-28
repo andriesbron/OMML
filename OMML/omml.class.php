d82a909a3d6e65a35afce1de94621a105c87ba73
andriesbron <andries@inourplace.nl>
2019-03-28 13:36:32 +0100

    Update omml.class.php

--- OMML/omml.class.php
+++ OMML/omml.class.php
@@ -2,7 +2,20 @@
 
 class omml
 {
-
+  private $guid;
+  private $src;
+  private $copyright;
+  private $access = "public";
+  
+  public static function getAccessLevels()
+  {
+    return array(
+      "public"
+      , "registered"
+      , "subscription"
+      , "contact"
+    );
+  }
 }
 
 ?>
